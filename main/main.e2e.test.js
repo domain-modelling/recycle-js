ee81ffaa2c9ccfcac7c7d23343ecf6941a2433a3
Michel Grootjans <michel.grootjans@gmail.com>
2023-05-31 09:14:28 +0200

    introduced the PriceCalculator

--- main/main.e2e.test.js
+++ main/main.e2e.test.js
@@ -1,6 +1,51 @@
 const axios = require('axios');
+const uuid = require('uuid');
 const express = require('express');
 const {routes} = require('./routes');
+
+function calculatePrice(cardId) {
+    return {
+        command_id: uuid.v3,
+        payload: {card_id: cardId},
+        type: 'CalculatePrice'
+    };
+}
+
+function idCardRegistered(cardId, personId = "John Doe", address = "an address",
+    street = "a street") {
+    return {
+        created_at: '2023-05-31T06:48:42.456824Z',
+        event_id: '33815a89-4f84-4a55-bbc2-8fab235c7475',
+        payload: {
+            card_id: cardId,
+            person_id: personId,
+            address: address,
+            street: street,
+        },
+        type: 'IdCardRegistered'
+    };
+}
+
+function idCardScannedAtEntranceGate(cardId, date = "2023-01-01") {
+    return {
+        created_at: '2023-05-31T06:48:42.456837Z',
+        event_id: '508a36e8-751e-4fd7-9887-6cf9be106f44',
+        payload: {
+            card_id: cardId,
+            date: date
+        },
+        type: 'IdCardScannedAtEntranceGate'
+    };
+}
+
+function idCardScannedAtExitGate(cardId) {
+    return {
+        created_at: '2023-05-31T06:48:42.456840Z',
+        event_id: '25773bfe-cab0-4dde-af97-9923502b475d',
+        payload: {card_id: cardId},
+        type: 'IdCardScannedAtExitGate'
+    };
+}
 
 describe("E2E happy flow", () => {
     let server;
@@ -25,11 +70,11 @@ describe("E2E happy flow", () => {
     it("POST /calculate without history", async () => {
         const calculation = await post("/handle-command", {
             history: [
-                // there should be something here
+                idCardRegistered('123'),
+                idCardScannedAtEntranceGate("123"),
+                idCardScannedAtExitGate("123")
             ],
-            command: {
-                // there should be something here too
-            }
+            command: calculatePrice('123'),
         });
         expect(calculation).toMatchObject(priceWasCalculated("123", 0));
     });
--- main/routes.js
+++ main/routes.js
@@ -4,6 +4,12 @@ const bodyParser = require('body-parser');
 const routes = Router();
 routes.use(bodyParser.json());
 
+class PriceCalculator {
+    calculatePrice(cardId) {
+        return 0;
+    }
+}
+
 routes.get("/", (request, response) => {
     return response.json({status: "ok"});
 });
@@ -13,14 +19,15 @@ routes.get("/validate", (request, response) => {
 });
 
 routes.post("/handle-command", (request, response) => {
-    const {history, command} = request.body
+    const { history, command } = request.body
+    const priceCalculator = new PriceCalculator()
     const answer = {
         event_id: "foo",
         created_at: new Date().toISOString(),
         type: "PriceWasCalculated",
         payload: {
             card_id: "123",
-            price_amount: 1,
+            price_amount: priceCalculator.calculatePrice("123"),
             price_currency: "EUR",
         },
     };
